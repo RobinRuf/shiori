8664767e27e9b9fdb6875b94f0488c8a279309e4
RobinRuf <robin.ruf@hotmail.com>
2025-02-15 20:58:14 +0100

    export type

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,8 +1,10 @@
-import type { MDXComponents } from 'mdx/types';
 import Image, { ImageProps } from 'next/image';
 import Card from './components/mdx/card';
 import CodeBlock from './components/mdx/codeblock';
 import ExternalLink from './components/mdx/external-link';
+import type { ComponentType } from 'react';
+
+export type MDXComponents = Partial<Record<string, ComponentType<any>>>;
 
 export function useMDXComponents(components?: MDXComponents): MDXComponents {
   const defaultComponents: MDXComponents = {
